a73d7ece4d52b70d9b609f4c34db269d22ea320a
Ashubajpai12345 <bajpaiashu857@gmail.com>
2025-09-20 23:35:50 +0530

    Update groupname.js

--- Priyansh/commands/groupname.js
+++ Priyansh/commands/groupname.js
@@ -5,7 +5,7 @@ module.exports.config = {
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Rename your group",
 	commandCategory: "Box", 
-	usages: "groupname [F9TIM9 KI CHU7🩷🥴]", 
+	usages: "groupname [name]", 
 	cooldowns: 0,
 	dependencies: [] 
 };
